d541a2ff5eff0f6510c87bc3bee596fc202972e5
Amra7 <amrapop@gmail.com>
2014-12-08 21:03:22 +0100

    Second

--- src/PrviPackage/ArrayUtil.java
+++ src/PrviPackage/ArrayUtil.java
@@ -33,64 +33,119 @@ public class ArrayUtil {
 
 		return newArray;
 	}
-	
+
 	/**
-	 * Funkcija koja  svakom clanu niza zadane velicine pripisuje vrijednost koju mi odredjumeo
-	 * @param size vrijedost koja oznacava velcinu niza
-	 * @param defaultValue je određena vrijednost koju sami definisemo
+	 * Funkcija koja svakom clanu niza zadane velicine pripisuje vrijednost koju
+	 * mi odredjumeo
+	 * 
+	 * @param size
+	 *            vrijedost koja oznacava velcinu niza
+	 * @param defaultValue
+	 *            je određena vrijednost koju sami definisemo
 	 * @return vraca novi niz
 	 */
-	public static int [] getArray( int size, int defaultValue){
-	 	int [] newArray = new int [size];
-		for ( int i = 0; i< newArray.length; i++){
-		newArray[i] = defaultValue;		
-	}
+	public static int[] getArray(int size, int defaultValue) {
+		int[] newArray = new int[size];
+		for (int i = 0; i < newArray.length; i++) {
+			newArray[i] = defaultValue;
+		}
 		return newArray;
 	}
-	
+
 	/**
-	 * Funkcija koja prima dva niza, a prepisuje vrijednosti iy prvog niza u drugi
-	 * @param niz1 prvi niz elemenata
-	 * @param niz2 drugi niz elemenata
-	 * @return vraca drugi niz elemenata u koji smo prepisali vrijednosti iz prvog niza elemenata
-	 * @throws IllegalArgumentException ako je prvi niz veci od drugog
+	 * Funkcija koja prima dva niza, a prepisuje vrijednosti iy prvog niza u
+	 * drugi
+	 * 
+	 * @param niz1
+	 *            prvi niz elemenata
+	 * @param niz2
+	 *            drugi niz elemenata
+	 * @return vraca drugi niz elemenata u koji smo prepisali vrijednosti iz
+	 *         prvog niza elemenata
+	 * @throws IllegalArgumentException
+	 *             ako je prvi niz veci od drugog
 	 */
-	public static int [] copyArray ( int [] array1, int [] array2){
-//		int [] newArray = new int [array1.length];
-	
-		if ( array2.length < array1.length ){
-		    array2 =  new int [array1.length];     //throw new IllegalArgumentException ("Ovaj program je nemoguce izvrsiti");	    
-		    
-//			for ( int i =0; i < array1.length; i++){
-//				newArray[i] = array1[i];
-//			}
-//			array2 = newArray;
-//		} else {
-				  
-//	}
-//			return array2;
-	   }
-		for ( int i =0; i < array1.length; i++){
-			array2[i] = array1[i];
-	    }
-		return array2;
-}
-	
-	public static int [] pomjeriDesno (int [] array){
-		
-		for ( int i=array.length-2; i>=0; i--){
-			array[i+1] = array[i];
+	public static int[] copyArray(int[] array1, int[] array2) {
+		// int [] newArray = new int [array1.length];
+
+		if (array2.length < array1.length) {
+			array2 = new int[array1.length]; // throw new
+												// IllegalArgumentException
+												// ("Ovaj program je nemoguce izvrsiti");
+
+			// for ( int i =0; i < array1.length; i++){
+			// newArray[i] = array1[i];
+			// }
+			// array2 = newArray;
+			// } else {
+
+			// }
+			// return array2;
 		}
-		array[0] =0;
+		for (int i = 0; i < array1.length; i++) {
+			array2[i] = array1[i];
+		}
+		return array2;
+	}
+
+	public static int[] pomjeriDesno(int[] array) {
+
+		for (int i = array.length - 2; i >= 0; i--) {
+			array[i + 1] = array[i];
+		}
+		array[0] = 0;
 		return array;
 	}
-	
-public static int [] pomjeriLijevo(int [] array){
-		
-		for ( int i= 1; i< array.length; i++){
-			array[i-1] = array[i];
+
+	public static int[] pomjeriLijevo(int[] array) {
+
+		for (int i = 1; i < array.length; i++) {
+			array[i - 1] = array[i];
 		}
-		array[array.length-1] =0;
+		array[array.length - 1] = 0;
 		return array;
+	}
+/**
+ *  Funkcija pomocu koje unosim vrijednosti u matricu
+ * @param size je velicina matrice koju mi unosimo
+ * @return vraca matricu sa popunjenim vrijednostima koje proizvoljno unosimo
+ */
+	public static int[][] makingMatrix(int size) {
+		int[][] matrix = new int[size][size];
+		for (int i = 0; i < matrix.length; i++) {
+			for (int j = 0; j < matrix[i].length; j++) {
+				System.out.println("Unesi vrijednosti u matricu: ");
+				matrix[i][j] = TextIO.getlnInt();
+			}
+		}
+		return matrix;
+	}
+/**
+ * Funkcija koja printa matricu, dvodimenzionalni niz
+ * @param matrix dvodimenzionalni niz 
+ */
+	public static void printMatrix(int[][] matrix) {
+		for (int i = 0; i < matrix.length; i++) {
+			for (int j = 0; j < matrix[i].length; j++) {
+				System.out.printf(" %2d ", matrix[i][j]);
+			}
+			System.out.println();
+		}
+	}
+/**
+ * Funkcija koja vraca niz velicina koji su velicine kolone matice
+ * @param matrix dvodimenzionalni niz
+ * @param numColumn broj kolone u matici
+ * @return vraca niz vrijednosti odredjene kolone iz matrice
+ */
+	public static int[] arrayOfColumn(int[][] matrix, int numColumn) {
+		int[] nizKolone = new int[matrix.length];
+		int j = numColumn;
+			for (int i = 0; i < matrix.length; i++) {
+				nizKolone[i] = matrix[i][j];
+			}
+		
+		return nizKolone;
+
 	}
 }
--- src/PrviPackage/TestnaKlasa.java
+++ src/PrviPackage/TestnaKlasa.java
@@ -7,15 +7,24 @@ public class TestnaKlasa {
     int [] arr2= ArrayUtil.getArray(3, 1);
     ArrayUtil.printArray(arr1);
     ArrayUtil.printArray(arr2);
-    
-  
-    
+      
     arr2 = ArrayUtil.copyArray(arr1, arr2);
 	ArrayUtil.printArray(arr2);
-	ArrayUtil.pomjeriDesno(arr1);
-	ArrayUtil.printArray(arr1);
-	ArrayUtil.pomjeriLijevo(arr1);
-	ArrayUtil.printArray(arr1);
+	arr2 =ArrayUtil.pomjeriDesno(arr1);
+	ArrayUtil.printArray(arr2);
+	arr2 =ArrayUtil.pomjeriLijevo(arr1);
+	ArrayUtil.printArray(arr2);
+	
+	System.out.println("Unesi velicniu matice: ");
+	int size = TextIO.getlnInt();
+	int [][] matrix = ArrayUtil.makingMatrix(size);
+	ArrayUtil.printMatrix(matrix);
+	
+	System.out.println("Unesi broj kolone: ");
+	int numColumn = TextIO.getlnInt();
+	int [] nizKolone = ArrayUtil.arrayOfColumn(matrix, numColumn);
+	ArrayUtil.printArray(nizKolone);
+	
 	
 	}
 			
